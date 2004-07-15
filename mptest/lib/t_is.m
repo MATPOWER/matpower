2a39e37b025d218a074e8f2496e0ec0b97317da5
Ray Zimmerman <rz10@cornell.edu>
2004-07-15 14:29:46 +0000

    Formatting.

--- mptest/lib/t_is.m
+++ mptest/lib/t_is.m
@@ -1,17 +1,19 @@
 function t_is(got, expected, prec, msg)
-	if nargin < 4
-		msg = '';
-	end
-	if nargin < 3 | isempty(prec)
-		prec = 5;
-	end
-	
-	cond = ( max(max(abs(got - expected))) < 10^(-prec) );
-	
-	t_ok(cond, msg);
-	if ~cond
-		got
-		expected
-		got_minus_expected = got - expected
-	end
+
+if nargin < 4
+	msg = '';
+end
+if nargin < 3 | isempty(prec)
+	prec = 5;
+end
+
+cond = ( max(max(abs(got - expected))) < 10^(-prec) );
+
+t_ok(cond, msg);
+if ~cond
+	got
+	expected
+	got_minus_expected = got - expected
+end
+
 return;
