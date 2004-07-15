808016bb7ec2a44f1a39eeef3ca64046647fcb6a
Ray Zimmerman <rz10@cornell.edu>
2004-07-15 15:27:50 +0000

    Added elapsed time.

--- mptest/lib/t_begin.m
+++ mptest/lib/t_begin.m
@@ -4,12 +4,14 @@ global t_num_of_tests;
 global t_counter;
 global t_ok_cnt;
 global t_not_ok_cnt;
+global t_clock;
+
+fprintf('1..%d\n', num_of_tests);
 
 t_num_of_tests = num_of_tests;
 t_counter = 1;
 t_ok_cnt = 0;
 t_not_ok_cnt = 0;
-
-fprintf('1..%d\n', num_of_tests);
+t_clock = clock;
 
 return;
--- mptest/lib/t_end.m
+++ mptest/lib/t_end.m
@@ -4,13 +4,16 @@ global t_num_of_tests;
 global t_counter;
 global t_ok_cnt;
 global t_not_ok_cnt;
+global t_clock;
 
 t_counter = t_counter - 1;
 
 if t_counter == t_num_of_tests & t_ok_cnt == t_counter & t_not_ok_cnt == 0
 	fprintf('All tests successful (%d of %d).\n', t_ok_cnt, t_num_of_tests);
 else
-	fprintf('Ran %d of %d tests: %d passed, %d failed\n', t_counter, t_num_of_tests, t_ok_cnt, t_not_ok_cnt);
+	fprintf('Ran %d of %d tests: %d passed, %d failed.\n', ...
+		t_counter, t_num_of_tests, t_ok_cnt, t_not_ok_cnt);
 end
+fprintf('Elapsed time %.2f seconds.\n', etime(clock, t_clock));
 
 return;
