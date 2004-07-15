c7043214f93e2388822eaf2e3d368513c333f698
Ray Zimmerman <rz10@cornell.edu>
2004-07-15 14:29:38 +0000

    Added ok and not_ok counters.

--- mptest/lib/t_ok.m
+++ mptest/lib/t_ok.m
@@ -1,13 +1,21 @@
 function t_ok(cond, msg)
-	global t_counter;
-	if nargin < 2 | strcmp(msg, '')
-		msg = '';
-	else
-		msg = [' - ', msg];
-	end
-	if ~cond
-		fprintf('not ');
-	end
-	fprintf('ok %d%s\n', t_counter, msg);
-	t_counter = t_counter + 1;
+
+global t_counter;
+global t_ok_cnt;
+global t_not_ok_cnt;
+
+if nargin < 2 | strcmp(msg, '')
+	msg = '';
+else
+	msg = [' - ', msg];
+end
+if cond
+	t_ok_cnt = t_ok_cnt + 1;
+else
+	t_not_ok_cnt = t_not_ok_cnt + 1;
+	fprintf('not ');
+end
+fprintf('ok %d%s\n', t_counter, msg);
+t_counter = t_counter + 1;
+
 return;
