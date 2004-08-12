e3315b75030c36bc4553e8d702e24239c56306b6
Ray Zimmerman <rz10@cornell.edu>
2004-08-12 20:13:10 +0000

    Changes from Carlos to allow for mixed cost types.

--- lib/totcost.m
+++ lib/totcost.m
@@ -2,7 +2,7 @@ function totalcost = totcost(gencost, Pg)
 %TOTCOST    Computes total cost for generators at given output level.
 %   totalcost = totcost(gencost, Pg) computes total cost for generators given
 %   a matrix in gencost format and a column vector or matrix of generation
-%   levels. The the return value has the same dimensions as Pg. Each row
+%   levels. The return value has the same dimensions as Pg. Each row
 %   of gencost is used to evaluate the cost at the points specified in the
 %   corresponding row of Pg.
 
@@ -18,21 +18,23 @@ function totalcost = totcost(gencost, Pg)
 totalcost = zeros(ng, size(Pg, 2));
 
 if ~isempty(gencost)
-	if gencost(1, MODEL) == PW_LINEAR
-		x = gencost(:, COST:2:(m-1));
-		y = gencost(:, (COST+1):2:m);
-		for i = 1:ng
-			if gencost(i, N) > 0
-				j1 = 1:(gencost(i, N) - 1);		j2 = 2:gencost(i, N);
-				pp = mkpp(x(i, 1:gencost(i, N))', [(y(i,j2) - y(i,j1)) ./ (x(i,j2) - x(i,j1));  y(i,j1)]');
-				totalcost(i,:) = ppval(pp, Pg(i,:));
-			end
-		end
-	elseif gencost(1, MODEL) == POLYNOMIAL
-		for i = 1:ng
-			totalcost(i,:) = polyval( gencost( i, COST:(COST+gencost(i, N)-1) ) , Pg(i,:) );
-		end
-	end
+  ipwl = find(gencost(:, MODEL) == PW_LINEAR);
+  ipol = find(gencost(:, MODEL) == POLYNOMIAL);
+  if ~isempty(ipwl)
+    x = gencost(:, COST:2:(m-1));
+    y = gencost(:, (COST+1):2:m);
+    for i = ipwl'
+      if gencost(i, N) > 0
+        j1 = 1:(gencost(i, N) - 1);    j2 = 2:gencost(i, N);
+        pp = mkpp(x(i, 1:gencost(i, N))', [(y(i,j2) - y(i,j1)) ./ (x(i,j2) - x(i,j1));  y(i,j1)]');
+        totalcost(i,:) = ppval(pp, Pg(i,:));
+      end
+    end
+  end
+  for i = ipol'
+    totalcost(i,:)= polyval(gencost(i, COST:(COST+gencost(i, N)-1) ), Pg(i,:) );
+  end
 end
 
 return;
+
