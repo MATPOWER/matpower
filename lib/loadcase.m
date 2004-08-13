4848e97ac9fd2427b27f694313c3b1c1092f8d7b
Ray Zimmerman <rz10@cornell.edu>
2004-08-13 20:33:23 +0000

    Made areas and gencost optional output variables.

--- lib/loadcase.m
+++ lib/loadcase.m
@@ -2,6 +2,7 @@ function [baseMVA, bus, gen, branch, areas, gencost, info] = loadcase(casefile)
 %LOADCASE   Load .m or .mat case files or data struct in MATPOWER format
 %
 %   [baseMVA, bus, gen, branch, areas, gencost ] = loadcase(casefile)
+%   [baseMVA, bus, gen, branch ] = loadcase(casefile)
 %
 %   Here casefile is either a struct containing the fields baseMVA, bus,
 %   gen, branch, areas, gencost, or a string containing the name of the file.
@@ -12,6 +13,7 @@ function [baseMVA, bus, gen, branch, areas, gencost, info] = loadcase(casefile)
 %   error message.  Alternatively, it can be called with the syntax:
 %
 %   [baseMVA, bus, gen, branch, areas, gencost, info] = loadcase(casefile)
+%   [baseMVA, bus, gen, branch, info] = loadcase(casefile)
 %
 %   In this case, the function will not abort, but info will contain an exit
 %   code as follows:
@@ -33,10 +35,12 @@ function [baseMVA, bus, gen, branch, areas, gencost, info] = loadcase(casefile)
 baseMVA = []; bus = []; gen = []; branch = []; areas = []; gencost = [];
 
 if isstruct(casefile)				%% first param is a struct
-	if nargout < 7
-		if ~isfield(casefile,'baseMVA') | ~isfield(casefile,'bus') | ...
-				~isfield(casefile,'gen') | ~isfield(casefile,'branch') | ...
-				~isfield(casefile,'areas') | ~isfield(casefile,'gencost') 
+	if ~( isfield(casefile,'baseMVA') & isfield(casefile,'bus') & ...
+			isfield(casefile,'gen') & isfield(casefile,'branch') ) | ...
+			(nargout > 4 & ~( isfield(casefile,'areas') & isfield(casefile,'gencost') ) )
+		if nargout < 7
+			error('loadcase: one or more of the data matrices is undefined');
+		else
 			info = 5;
 			return;
 		end
@@ -45,8 +49,10 @@ if isstruct(casefile)				%% first param is a struct
 	bus		= casefile.bus;
 	gen		= casefile.gen;
 	branch	= casefile.branch;
-	areas	= casefile.areas;
-	gencost	= casefile.gencost;
+	if nargout > 5
+		areas	= casefile.areas;
+		gencost	= casefile.gencost;
+	end
 	info	= 0;
 	return;
 else								%% OK, better be a string, else give up
@@ -63,59 +69,69 @@ end
 %% check for explicit extension
 l = length(casefile);
 if l > 2
-  if strcmp(casefile(l-1:l), '.m')
-    rootname = casefile(1:l-2);
-    extension = '.m';
-  elseif l > 4
-    if strcmp(casefile(l-3:l), '.mat')
-       rootname = casefile(1:l-4);
-       extension = '.mat';
-    end
-  end
+	if strcmp(casefile(l-1:l), '.m')
+		rootname = casefile(1:l-2);
+		extension = '.m';
+	elseif l > 4
+		if strcmp(casefile(l-3:l), '.mat')
+			rootname = casefile(1:l-4);
+			extension = '.mat';
+		end
+	end
 end
 
 %% get data from file
 if exist('rootname') ~= 1			%% no explicit extension
-  if exist([casefile '.mat']) == 2
-     load(casefile);
-  elseif exist([casefile '.m']) == 2
-    [baseMVA, bus, gen, branch, areas, gencost] = feval(casefile);
-  else
-    if nargout < 7
-      error('loadcase: specified case not in MATLAB''s search path');
-    else
-      info = 2;
-      return;
-    end
-  end
+	if exist([casefile '.mat']) == 2
+		load(casefile);
+	elseif exist([casefile '.m']) == 2
+		if nargout > 5
+			[baseMVA, bus, gen, branch, areas, gencost] = feval(casefile);
+		else
+			[baseMVA, bus, gen, branch] = feval(casefile);
+		end
+	else
+		if nargout < 7
+			error('loadcase: specified case not in MATLAB''s search path');
+		else
+			info = 2;
+			return;
+		end
+	end
 else								%% explicit extension given
-  if strcmp(extension,'.mat')
-    if exist([rootname '.mat']) == 2
-      load(rootname) ;
-    else
-      if nargout < 7
-        error('loadcase: specified MAT file does not exist');
-      else
-        info = 3;
-        return;
-      end
-    end
-  elseif strcmp(extension,'.m')
-    if exist([rootname '.m']) == 2
-      [baseMVA, bus, gen, branch, areas, gencost] = feval(rootname);
-    else
-      if nargout < 7
-        error('loadcase: specified M file does not exist');
-      else
-        info = 4;
-        return;
-      end
-    end
-  end
+	if strcmp(extension,'.mat')
+		if exist([rootname '.mat']) == 2
+			load(rootname) ;
+		else
+			if nargout < 7
+				error('loadcase: specified MAT file does not exist');
+			else
+				info = 3;
+				return;
+			end
+		end
+	elseif strcmp(extension,'.m')
+		if exist([rootname '.m']) == 2
+			if nargout > 5
+				[baseMVA, bus, gen, branch, areas, gencost] = feval(rootname);
+			else
+				[baseMVA, bus, gen, branch] = feval(rootname);
+			end
+		else
+			if nargout < 7
+				error('loadcase: specified M file does not exist');
+			else
+				info = 4;
+				return;
+			end
+		end
+	end
 end
 
+%% check for undefined data matrices
 if ~( exist('baseMVA') == 1 & exist('bus') == 1 & exist('branch') == 1 ...
-		& exist('gen') == 1 & exist('areas') == 1 & exist('gencost') == 1)
+		& exist('gen') == 1 ) | ...
+		( nargout > 5 & ~(exist('areas') == 1 & exist('gencost') == 1) )
   	if nargout < 7
     	error('loadcase: one or more of the data matrices is undefined');
   	else
@@ -123,6 +139,10 @@ if ~( exist('baseMVA') == 1 & exist('bus') == 1 & exist('branch') == 1 ...
   	end
 else
   	info = 0;
+end
+
+if nargout < 6
+	areas = info;
 end
 
 return;
