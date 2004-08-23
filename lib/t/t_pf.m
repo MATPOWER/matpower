c350f1a7df3c5b6112effb6ec656202d2e940eac
Ray Zimmerman <rz10@cornell.edu>
2004-08-23 16:01:22 +0000

    Add tests for runpf.

---
+++ lib/t/t_pf.m
@@ -0,0 +1,76 @@
+function t_pf(quiet)
+%T_PF  Tests for simple power flow.
+
+%   MATPOWER
+%   $Id$
+%   by Ray Zimmerman, PSERC Cornell
+%   Copyright (c) 2004 by Power System Engineering Research Center (PSERC)
+%   See http://www.pserc.cornell.edu/ for more info.
+
+if nargin < 1
+	quiet = 0;
+end
+
+t_begin(20, quiet);
+
+casefile = 't_case_9';
+if quiet
+	verbose = 0;
+else
+	verbose = 1;
+end
+mpopt = mpoption('OUT_ALL', 0, 'VERBOSE', verbose);
+
+%% get solved AC power flow case from MAT-file
+load soln9_pf;		%% defines bus_soln, gen_soln, branch_soln
+
+%% run Newton PF
+t = 'Newton PF : ';
+mpopt = mpoption(mpopt, 'PF_ALG', 1);
+[baseMVA, bus, gen, branch, success, et] = runpf('t_case9_pf', mpopt);
+t_ok(success, [t 'success']);
+t_is(bus, bus_soln, 6, [t 'bus']);
+t_is(gen, gen_soln, 6, [t 'gen']);
+t_is(branch, branch_soln, 6, [t 'branch']);
+
+%% run fast-decoupled PF (XB version)
+t = 'Fast Decoupled (XB) PF : ';
+mpopt = mpoption(mpopt, 'PF_ALG', 2);
+[baseMVA, bus, gen, branch, success, et] = runpf('t_case9_pf', mpopt);
+t_ok(success, [t 'success']);
+t_is(bus, bus_soln, 6, [t 'bus']);
+t_is(gen, gen_soln, 6, [t 'gen']);
+t_is(branch, branch_soln, 6, [t 'branch']);
+
+%% run fast-decoupled PF (BX version)
+t = 'Fast Decoupled (BX) PF : ';
+mpopt = mpoption(mpopt, 'PF_ALG', 3);
+[baseMVA, bus, gen, branch, success, et] = runpf('t_case9_pf', mpopt);
+t_ok(success, [t 'success']);
+t_is(bus, bus_soln, 6, [t 'bus']);
+t_is(gen, gen_soln, 6, [t 'gen']);
+t_is(branch, branch_soln, 6, [t 'branch']);
+
+%% run Gauss-Seidel PF
+t = 'Gauss-Seidel PF : ';
+mpopt = mpoption(mpopt, 'PF_ALG', 4);
+[baseMVA, bus, gen, branch, success, et] = runpf('t_case9_pf', mpopt);
+t_ok(success, [t 'success']);
+t_is(bus, bus_soln, 5, [t 'bus']);
+t_is(gen, gen_soln, 5, [t 'gen']);
+t_is(branch, branch_soln, 5, [t 'branch']);
+
+%% get solved AC power flow case from MAT-file
+load soln9_dcpf;		%% defines bus_soln, gen_soln, branch_soln
+
+%% run DC PF
+t = 'DC PF : ';
+[baseMVA, bus, gen, branch, success, et] = rundcpf('t_case9_pf', mpopt);
+t_ok(success, [t 'success']);
+t_is(bus, bus_soln, 6, [t 'bus']);
+t_is(gen, gen_soln, 6, [t 'gen']);
+t_is(branch, branch_soln, 6, [t 'branch']);
+
+t_end;
+
+return;
