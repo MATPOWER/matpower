aa9e8f06a44a07977bb8c7071fd1e9dbd98054dc
Ray Zimmerman <rz10@cornell.edu>
2008-06-10 17:44:01 +0000

    Add.

---
+++ lib/@opf_model/get_mpc.m
@@ -0,0 +1,12 @@
+function mpc = get_mpc(om)
+%GET_MPC  Returns the MATPOWER case struct.
+
+%   MATPOWER
+%   $Id$
+%   by Ray Zimmerman, PSERC Cornell
+%   Copyright (c) 2008 by Power System Engineering Research Center (PSERC)
+%   See http://www.pserc.cornell.edu/matpower/ for more info.
+
+mpc = om.mpc;
+
+return;
