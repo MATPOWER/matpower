658740887c6cd9533a5544e6fb78b15f55def545
Ray Zimmerman <rz10@cornell.edu>
2008-06-10 18:00:15 +0000

    Updated to use struct return value for opf_args (to handle userfcn).

--- lib/dcopf.m
+++ lib/dcopf.m
@@ -9,12 +9,8 @@ function [varargout] = dcopf(varargin)
 %   Copyright (c) 1996-2008 by Power System Engineering Research Center (PSERC)
 %   See http://www.pserc.cornell.edu/matpower/ for more info.
 
-[baseMVA, bus, gen, branch, areas, gencost, Au, lbu, ubu, mpopt, ...
-    N, fparm, H, Cw, z0, zl, zu] = opf_args(varargin{:});
-
+[mpc, mpopt] = opf_args(varargin{:});
 mpopt = mpoption(mpopt, 'PF_DC', 10);
-[varargout{1:nargout}] = opf(baseMVA, bus, gen, branch, ...
-                areas, gencost, Au, lbu, ubu, mpopt, ...
-                N, fparm, H, Cw, z0, zl, zu);
+[varargout{1:nargout}] = opf(mpc, mpopt);
 
 return;
--- lib/fmincopf.m
+++ lib/fmincopf.m
@@ -10,12 +10,8 @@ function [varargout] = fmincopf(varargin)
 %   Copyright (c) 2000-2008 by Power System Engineering Research Center (PSERC)
 %   See http://www.pserc.cornell.edu/matpower/ for more info.
 
-[baseMVA, bus, gen, branch, areas, gencost, Au, lbu, ubu, mpopt, ...
-    N, fparm, H, Cw, z0, zl, zu] = opf_args(varargin{:});
-
+[mpc, mpopt] = opf_args(varargin{:});
 mpopt = mpoption(mpopt, 'OPF_ALG', 520);
-[varargout{1:nargout}] = opf(baseMVA, bus, gen, branch, ...
-                areas, gencost, Au, lbu, ubu, mpopt, ...
-                N, fparm, H, Cw, z0, zl, zu);
+[varargout{1:nargout}] = opf(mpc, mpopt);
 
 return;
