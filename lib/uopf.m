73d5118f685f6cdb5071a72956944d36b2f96e95
Ray Zimmerman <rz10@cornell.edu>
2008-07-03 17:15:33 +0000

    Updated so input/output args match opf.m.

--- lib/uopf.m
+++ lib/uopf.m
@@ -1,27 +1,37 @@
-function [bus0, gen0, branch0, f0, success0, et] = ...
-        uopf(baseMVA, bus, gen, branch, areas, gencost, Au, lbu, ubu, mpopt, ...
-        N, fparm, H, Cw, z0, zl, zu)
+function [bus, gen, branch, f, success, info, et, g, jac, xr, pimul] = ...
+    uopf(varargin)
 %UOPF  Solves combined unit decommitment / optimal power flow.
 %
-%   [bus, gen, branch, f, success] = uopf(casefile, mpopt)
+%   Returns either a results struct and optinally a success flag, or individual
+%   data matrices, the objective function value and a success flag. In the
+%   latter case, there are additional optional return values.
 %
-%   [bus, gen, branch, f, success] = uopf(casefile, A, l, u, mpopt)
+%   results = uopf(...)
+%   [results, success] = uopf(...)
+%   [bus, gen, branch, f, success] = uopf(...)
+%   [bus, gen, branch, f, success, info, et, g, jac, xr, pimul] = uopf(...)
 %
-%   [bus, gen, branch, f, success] = uopf(baseMVA, bus, gen, branch, ...
-%                                    areas, gencost, mpopt)
+%   Input arguments options are as follows:
 %
-%   [bus, gen, branch, f, success] = uopf(baseMVA, bus, gen, branch, ...
-%                                    areas, gencost, A, l, u, mpopt)
+%   uopf(mpc)
+%   uopf(mpc, mpopt)
+%   uopf(mpc, userfcn, mpopt)
+%   uopf(mpc, A, l, u)
+%   uopf(mpc, A, l, u, mpopt)
+%   uopf(mpc, A, l, u, mpopt, N, fparm, H, Cw)
+%   uopf(mpc, A, l, u, mpopt, N, fparm, H, Cw, z0, zl, zu)
 %
-%   [bus, gen, branch, f, success] = uopf(baseMVA, bus, gen, branch, ...
-%                                    areas, gencost, A, l, u, mpopt, ...
-%                                    N, fparm, H, Cw)
+%   uopf(baseMVA, bus, gen, branch, areas, gencost)
+%   uopf(baseMVA, bus, gen, branch, areas, gencost, mpopt)
+%   uopf(baseMVA, bus, gen, branch, areas, gencost, userfcn, mpopt)
+%   uopf(baseMVA, bus, gen, branch, areas, gencost, A, l, u)
+%   uopf(baseMVA, bus, gen, branch, areas, gencost, A, l, u, mpopt)
+%   uopf(baseMVA, bus, gen, branch, areas, gencost, A, l, u, ...
+%                               mpopt, N, fparm, H, Cw)
+%   uopf(baseMVA, bus, gen, branch, areas, gencost, A, l, u, ...
+%                               mpopt, N, fparm, H, Cw, z0, zl, zu)
 %
-%   [bus, gen, branch, f, success] = uopf(baseMVA, bus, gen, branch, ...
-%                                    areas, gencost, A, l, u, mpopt, ...
-%                                    N, fparm, H, Cw, z0, zl, zu)
-%
-%   [bus, gen, branch, f, success, et] = uopf(casefile)
+%   See 'help opf' for more information on input and output arguments.
 %
 %   Solves a combined unit decommitment and optimal power flow for a single
 %   time period. Uses an algorithm similar to dynamic programming. It proceeds
@@ -32,7 +42,7 @@ function [bus0, gen0, branch0, f0, success0, et] = ...
 %   continuing until there are no more candidates to be shut down or no
 %   more improvement can be gained by shutting something down.
 %   If VERBOSE in mpopt (see 'help mpoption') is true, it prints progress
-%   info, if it's > 1 it prints the output of each individual opf.
+%   info, if it is > 1 it prints the output of each individual opf.
 
 %   MATPOWER
 %   $Id$
@@ -40,177 +50,16 @@ function [bus0, gen0, branch0, f0, success0, et] = ...
 %   Copyright (c) 1996-2008 by Power System Engineering Research Center (PSERC)
 %   See http://www.pserc.cornell.edu/matpower/ for more info.
 
-% Sort out input arguments
-if isstr(baseMVA) | isstruct(baseMVA)   % passing filename or struct
-  %---- uopf(baseMVA,  bus, gen, branch, areas, gencost, Au,    lbu, ubu, mpopt, N,  fparm, H, Cw, z0, zl, zu)
-  % 12  uopf(casefile, Au,  lbu, ubu,    mpopt, N,       fparm, H,   Cw,  z0,    zl, zu)
-  % 9   uopf(casefile, Au,  lbu, ubu,    mpopt, N,       fparm, H,   Cw)
-  % 5   uopf(casefile, Au,  lbu, ubu,    mpopt)
-  % 4   uopf(casefile, Au,  lbu, ubu)
-  % 2   uopf(casefile, mpopt)
-  % 1   uopf(casefile)
-  if any(nargin == [1, 2, 4, 5, 9, 12])
-    casefile = baseMVA;
-    if nargin == 12
-      zu    = fparm;
-      zl    = N;
-      z0    = mpopt;
-      Cw    = ubu;
-      H     = lbu;
-      fparm = Au;
-      N     = gencost;
-      mpopt = areas;
-      ubu   = branch;
-      lbu   = gen;
-      Au    = bus;
-    elseif nargin == 9
-      zu    = [];
-      zl    = [];
-      z0    = [];
-      Cw    = ubu;
-      H     = lbu;
-      fparm = Au;
-      N     = gencost;
-      mpopt = areas;
-      ubu   = branch;
-      lbu   = gen;
-      Au    = bus;
-    elseif nargin == 5
-      zu    = [];
-      zl    = [];
-      z0    = [];
-      Cw    = [];
-      H     = [];
-      fparm = [];
-      N     = [];
-      mpopt = areas;
-      ubu   = branch;
-      lbu   = gen;
-      Au    = bus;
-    elseif nargin == 4
-      zu    = [];
-      zl    = [];
-      z0    = [];
-      Cw    = [];
-      H     = [];
-      fparm = [];
-      N     = [];
-      mpopt = mpoption;
-      ubu   = branch;
-      lbu   = gen;
-      Au    = bus;
-    elseif nargin == 2
-      zu    = [];
-      zl    = [];
-      z0    = [];
-      Cw    = [];
-      H     = [];
-      fparm = [];
-      N     = [];
-      mpopt = bus;
-      ubu   = [];
-      lbu   = [];
-      Au    = sparse(0,0);
-    elseif nargin == 1
-      zu    = [];
-      zl    = [];
-      z0    = [];
-      Cw    = [];
-      H     = [];
-      fparm = [];
-      N     = [];
-      mpopt = mpoption;
-      ubu   = [];
-      lbu   = [];
-      Au    = sparse(0,0);
-    end
-  else
-    error('uopf.m: Incorrect input parameter order, number or type');
-  end
-  [baseMVA, bus, gen, branch, areas, gencost] = loadcase(casefile);
-else    % passing individual data matrices
-  %---- uopf(baseMVA, bus, gen, branch, areas, gencost, Au,   lbu, ubu, mpopt, N, fparm, H, Cw, z0, zl, zu)
-  % 17  uopf(baseMVA, bus, gen, branch, areas, gencost, Au,   lbu, ubu, mpopt, N, fparm, H, Cw, z0, zl, zu)
-  % 14  uopf(baseMVA, bus, gen, branch, areas, gencost, Au,   lbu, ubu, mpopt, N, fparm, H, Cw)
-  % 10  uopf(baseMVA, bus, gen, branch, areas, gencost, Au,   lbu, ubu, mpopt)
-  % 9   uopf(baseMVA, bus, gen, branch, areas, gencost, Au,   lbu, ubu)
-  % 7   uopf(baseMVA, bus, gen, branch, areas, gencost, mpopt)
-  % 6   uopf(baseMVA, bus, gen, branch, areas, gencost)
-  if any(nargin == [6, 7, 9, 10, 14, 17])
-    if nargin == 14
-      zu    = [];
-      zl    = [];
-      z0    = [];
-    elseif nargin == 10
-      zu    = [];
-      zl    = [];
-      z0    = [];
-      Cw    = [];
-      H     = [];
-      fparm = [];
-      N     = [];
-    elseif nargin == 9
-      zu    = [];
-      zl    = [];
-      z0    = [];
-      Cw    = [];
-      H     = [];
-      fparm = [];
-      N     = [];
-      mpopt = mpoption;
-    elseif nargin == 7
-      zu    = [];
-      zl    = [];
-      z0    = [];
-      Cw    = [];
-      H     = [];
-      fparm = [];
-      N     = [];
-      mpopt = Au;
-      ubu   = [];
-      lbu   = [];
-      Au    = sparse(0,0);
-    elseif nargin == 6
-      zu    = [];
-      zl    = [];
-      z0    = [];
-      Cw    = [];
-      H     = [];
-      fparm = [];
-      N     = [];
-      mpopt = mpoption;
-      ubu   = [];
-      lbu   = [];
-      Au    = sparse(0,0);
-    end
-  else
-    error('uopf.m: Incorrect input parameter order, number or type');
-  end
-end
-if size(N, 1) > 0
-  if size(N, 1) ~= size(fparm, 1) | size(N, 1) ~= size(H, 1) | ...
-     size(N, 1) ~= size(H, 2) | size(N, 1) ~= length(Cw)
-    error('uopf.m: wrong dimensions in generalized cost parameters');
-  end
-  if size(Au, 1) > 0 & size(N, 2) ~= size(Au, 2)
-    error('uopf.m: A and N must have the same number of columns');
-  end
-end
-if isempty(mpopt)
-  mpopt = mpoption;
-end
-
 %%----- initialization -----
-count       = 0;
-i           = 0;    %% this is to work around a bug in Matlab (4 and 5)
+t0 = clock;                                 %% start timer
+count   = 0;
+i       = 0;    %% this is to work around a bug in Matlab (4 and 5)
 
-%% default arguments
-if nargin < 6
-    mpopt = mpoption;                   %% use default options
-end
+%% process input arguments
+[mpc, mpopt] = opf_args(varargin{:});
 
 %% options
-verbose = mpopt(31);
+verbose = mpopt(31);    %% VERBOSE
 if verbose      %% turn down verbosity one level for calls to opf
     mpopt = mpoption(mpopt, 'VERBOSE', verbose-1);
 end
@@ -224,16 +73,15 @@ end
 [PW_LINEAR, POLYNOMIAL, MODEL, STARTUP, SHUTDOWN, NCOST, COST] = idx_cost;
 
 %%-----  do combined unit commitment/optimal power flow  -----
-t0 = clock;                                 %% start timer
 
 %% check for sum(Pmin) > total load, decommit as necessary
-on   = find( gen(:, GEN_STATUS) > 0 & ~isload(gen) );   %% gens in service
-onld = find( gen(:, GEN_STATUS) > 0 &  isload(gen) );   %% disp loads in serv
-load_capacity = sum(bus(:, PD)) - sum(gen(onld, PMIN)); %% total load capacity
-Pmin = gen(on, PMIN);
+on   = find( mpc.gen(:, GEN_STATUS) > 0 & ~isload(mpc.gen) );   %% gens in service
+onld = find( mpc.gen(:, GEN_STATUS) > 0 &  isload(mpc.gen) );   %% disp loads in serv
+load_capacity = sum(mpc.bus(:, PD)) - sum(mpc.gen(onld, PMIN)); %% total load capacity
+Pmin = mpc.gen(on, PMIN);
 while sum(Pmin) > load_capacity
     %% shut down most expensive unit
-    avgPmincost = totcost(gencost(on, :), Pmin) ./ Pmin;
+    avgPmincost = totcost(mpc.gencost(on, :), Pmin) ./ Pmin;
     [junk, i] = fairmax(avgPmincost);   %% pick one with max avg cost at Pmin
     i = on(i);                          %% convert to generator index
 
@@ -242,37 +90,26 @@ while sum(Pmin) > load_capacity
     end
 
     %% set generation to zero
-    gen(i, PG)          = 0;
-    gen(i, QG)          = 0;
-    gen(i, GEN_STATUS)  = 0;
+    mpc.gen(i, [ PG QG GEN_STATUS ]) = 0;
     
     %% update minimum gen capacity
-    on  = find( gen(:, GEN_STATUS) > 0 & ~isload(gen) );   %% gens in service
-    Pmin = gen(on, PMIN);
+    on  = find( mpc.gen(:, GEN_STATUS) > 0 & ~isload(mpc.gen) );   %% gens in service
+    Pmin = mpc.gen(on, PMIN);
 end
 
 %% run initial opf
-[bus, gen, branch, f, success, info, et] = opf(baseMVA, bus, gen, branch, ...
-                areas, gencost, Au, lbu, ubu, ...
-                mpopt, N, fparm, H, Cw, z0, zl, zu);
+[results, success] = opf(mpc, mpopt);
 
 %% best case so far
-bus1 = bus;
-gen1 = gen;
-branch1 = branch;
-success1 = success;
-f1 = f;
+results1 = results;
 
 %% best case for this stage (ie. with n gens shut down, n=0,1,2 ...)
-bus0 = bus1;
-gen0 = gen1;
-branch0 = branch1;
-success0 = success1;
-f0 = f1;
+results0 = results1;
+mpc.bus = results0.bus;     %% use these V as starting point for OPF
 
 while 1
     %% get candidates for shutdown
-    candidates = find(gen0(:, MU_PMIN) > 0 & gen0(:, PMIN) > 0);
+    candidates = find(results0.gen(:, MU_PMIN) > 0 & results0.gen(:, PMIN) > 0);
     if isempty(candidates)
         break;
     end
@@ -281,25 +118,17 @@ while 1
     for i = 1:length(candidates)
         k = candidates(i);
         %% start with best for this stage
-        gen = gen0;
+        mpc.gen = results0.gen;
         
         %% shut down gen k
-        gen(k, PG)          = 0;
-        gen(k, QG)          = 0;
-        gen(k, GEN_STATUS)  = 0;
+        mpc.gen(k, [ PG QG GEN_STATUS ]) = 0;
         
         %% run opf
-        [bus, gen, branch, f, success, info, et] = opf(baseMVA, bus0, gen, branch0, ...
-                                        areas, gencost, Au, lbu, ubu, ...
-                                        mpopt, N, fparm, H, Cw, z0, zl, zu);
+        [results, success] = opf(mpc, mpopt);
         
         %% something better?
-        if success & f < f1
-            bus1 = bus;
-            gen1 = gen;
-            branch1 = branch;
-            success1 = success;
-            f1 = f;
+        if success && results.f < results1.f
+            results1 = results;
             k1 = k;
             done = 0;   %% make sure we check for further decommitment
         end
@@ -314,15 +143,34 @@ while 1
             fprintf('Shutting down generator %d.\n', k1);
         end
         
-        bus0 = bus1;
-        gen0 = gen1;
-        branch0 = branch1;
-        success0 = success1;
-        f0 = f1;
+        results0 = results1;
+        mpc.bus = results0.bus;     %% use these V as starting point for OPF
     end 
 end
 
 %% compute elapsed time
 et = etime(clock, t0);
+
+%% finish preparing output
+if nargout > 0
+  success = results0.success;
+  if nargout <= 2
+    results0.et = et;
+    bus = results0;
+    gen = success;
+  else
+    [bus, gen, branch, f, info, xr, pimul] = deal(results0.bus, results0.gen, ...
+                    results0.branch, results0.f, results0.raw.info, ...
+                    results0.raw.xr, results0.raw.pimul);
+    if isfield(results0, 'g')
+      g = results0.g;
+    end
+    if isfield(results0, 'dg')
+      jac = results0.dg;
+    end
+  end
+elseif results0.success
+  printpf(mpc.baseMVA, results0.bus, results0.gen, results0.branch, results0.f, success, et, 1, mpopt);
+end
 
 return;
