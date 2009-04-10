2b729d55de94408d88a8fbdc4b8e1cde6f03f966
Ray Zimmerman <rz10@cornell.edu>
2009-04-10 19:17:16 +0000

    Rename constants_all.m to define_constants.m, call the idx_* files directly.

---
+++ lib/define_constants.m
@@ -0,0 +1,28 @@
+%DEFINE_CONSTANTS  Defines constants for named column indices to all data matrices.
+%
+%   define_constants
+%
+%   Script file that calls idx_area, idx_brch, idx_bus, idx_cost and
+%   idx_gen to define constants, especially those for the named column
+%   indices for the corresponding data matrices.
+
+%   MATPOWER
+%   $Id$
+%   by Ray Zimmerman, PSERC Cornell
+%   Copyright (c) 2009 by Power System Engineering Research Center (PSERC)
+%   See http://www.pserc.cornell.edu/matpower/ for more info.
+
+[AREA_I, PRICE_REF_BUS] = idx_area;
+
+[F_BUS, T_BUS, BR_R, BR_X, BR_B, RATE_A, RATE_B, RATE_C, ...
+    TAP, SHIFT, BR_STATUS, PF, QF, PT, QT, MU_SF, MU_ST, ...
+    ANGMIN, ANGMAX, MU_ANGMIN, MU_ANGMAX] = idx_brch;
+
+[PQ, PV, REF, NONE, BUS_I, BUS_TYPE, PD, QD, GS, BS, BUS_AREA, VM, ...
+    VA, BASE_KV, ZONE, VMAX, VMIN, LAM_P, LAM_Q, MU_VMAX, MU_VMIN] = idx_bus;
+
+[PW_LINEAR, POLYNOMIAL, MODEL, STARTUP, SHUTDOWN, NCOST, COST] = idx_cost;
+
+[GEN_BUS, PG, QG, QMAX, QMIN, VG, MBASE, GEN_STATUS, PMAX, PMIN, ...
+    MU_PMAX, MU_PMIN, MU_QMAX, MU_QMIN, PC1, PC2, QC1MIN, QC1MAX, ...
+    QC2MIN, QC2MAX, RAMP_AGC, RAMP_10, RAMP_30, RAMP_Q, APF] = idx_gen;
