7227d87daaaae1f54d44fe9d38d0befeb399713e
Ray Zimmerman <rz10@cornell.edu>
2010-01-25 19:02:17 +0000

    Removed obsolete check for 'qp'.

--- lib/t/t_opf_dc_ot.m
+++ lib/t/t_opf_dc_ot.m
@@ -38,7 +38,7 @@ mpopt = mpoption(mpopt, 'OPF_ALG_DC', 300);
 warning off optim:linprog:IgnoreStartPoint;
 
 %% run DC OPF
-if have_fcn('quadprog') || have_fcn('qp')
+if have_fcn('quadprog')
     %% set up indices
     ib_data     = [1:BUS_AREA BASE_KV:VMIN];
     ib_voltage  = [VM VA];
