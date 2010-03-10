295408fe441626e77ac63120684c3633882af35a
Ray Zimmerman <rz10@cornell.edu>
2010-03-10 18:08:32 +0000

    Massive help text update to more closely match MathWorks conventions ... function names in ALL CAPS,  See also ..., Examples, etc. and de-tab cleanup.

--- lib/qps_mips.m
+++ lib/qps_mips.m
@@ -1,42 +1,28 @@
 function [x, f, eflag, output, lambda] = qps_mips(H, c, A, l, u, xmin, xmax, x0, opt)
-%QPS_MIPS  Quadratic Program Solver based on MIPS
+%QPS_MIPS  Quadratic Program Solver based on MIPS.
+%   [X, F, EXITFLAG, OUTPUT, LAMBDA] = ...
+%       QPS_MIPS(H, C, A, L, U, XMIN, XMAX, X0, OPT)
 %   Uses the Matlab Interior Point Solver (MIPS) to solve the following
 %   QP (quadratic programming) problem:
 %
-%       min 1/2 x'*H*x + c'*x
-%        x
+%       min 1/2 X'*H*X + C'*X
+%        X
 %
 %   subject to
 %
-%       l <= A*x <= u       (linear constraints)
-%       xmin <= x <= xmax   (variable bounds)
+%       L <= A*X <= U       (linear constraints)
+%       XMIN <= X <= XMAX   (variable bounds)
 %
-%   [x, f, exitflag, output, lambda] = ...
-%       qps_mips(H, c, A, l, u, xmin, xmax, x0, opt)
-%
-%   x = qps_mips(H, c, A, l, u)
-%   x = qps_mips(H, c, A, l, u, xmin, xmax)
-%   x = qps_mips(H, c, A, l, u, xmin, xmax, x0)
-%   x = qps_mips(H, c, A, l, u, xmin, xmax, x0, opt)
-%   x = qps_mips(problem), where problem is a struct with fields:
-%                       H, c, A, l, u, xmin, xmax, x0, opt
-%                       all fields except 'H', 'c', 'A' and 'l' are optional
-%   x = qps_mips(...)
-%   [x, f] = qps_mips(...)
-%   [x, f, exitflag] = qps_mips(...)
-%   [x, f, exitflag, output] = qps_mips(...)
-%   [x, f, exitflag, output, lambda] = qps_mips(...)
-%
-%   Inputs:
+%   Inputs (all optional except H, C, A and L):
 %       H : matrix (possibly sparse) of quadratic cost coefficients
-%       c : vector of linear cost coefficients
-%       A, l, u : define the optional linear constraints. Default
-%           values for the elements of l and u are -Inf and Inf,
+%       C : vector of linear cost coefficients
+%       A, L, U : define the optional linear constraints. Default
+%           values for the elements of L and U are -Inf and Inf,
 %           respectively.
-%       xmin, xmax : optional lower and upper bounds on the
-%           x variables, defaults are -Inf and Inf, respectively.
-%       x0 : optional starting value of optimization vector x
-%       opt : optional options structure with the following fields,
+%       XMIN, XMAX : optional lower and upper bounds on the
+%           X variables, defaults are -Inf and Inf, respectively.
+%       X0 : optional starting value of optimization vector X
+%       OPT : optional options structure with the following fields,
 %           all of which are also optional (default values shown in
 %           parentheses)
 %           verbose (0) - controls level of progress output displayed
@@ -60,38 +46,70 @@ function [x, f, eflag, output, lambda] = qps_mips(H, c, A, l, u, xmin, xmax, x0,
 %               function so that it can appropriately scale the
 %               objective function term in the Hessian of the
 %               Lagrangian.
-%       problem : The inputs can alternatively be supplied in a single
-%           struct with fields corresponding to the input arguments
+%       PROBLEM : The inputs can alternatively be supplied in a single
+%           PROBLEM struct with fields corresponding to the input arguments
 %           described above: H, c, A, l, u, xmin, xmax, x0, opt
 %
 %   Outputs:
-%       x : solution vector
-%       f : final objective function value
-%       exitflag : exit flag,
+%       X : solution vector
+%       F : final objective function value
+%       EXITFLAG : exit flag
 %           1 = first order optimality conditions satisfied
 %           0 = maximum number of iterations reached
 %           -1 = numerically failed
-%       output : structure with fields:
+%       OUTPUT : output struct with the following fields:
 %           iterations - number of iterations performed
 %           hist - struct array with trajectories of the following:
 %                   feascond, gradcond, compcond, costcond, gamma,
 %                   stepsize, obj, alphap, alphad
 %           message - exit message
-%       lambda : struct containing the Langrange and Kuhn-Tucker
+%       LAMBDA : struct containing the Langrange and Kuhn-Tucker
 %           multipliers on the constraints, with fields:
-%           mu_l - lower bound on linear constraints
-%           mu_u - upper bound on linear constraints
+%           mu_l - lower (left-hand) limit on linear constraints
+%           mu_u - upper (right-hand) limit on linear constraints
 %           lower - lower bound on optimization variables
 %           upper - upper bound on optimization variables
 %
-%   Note the calling syntax is almost identical to that of 'quadprog'
+%   Note the calling syntax is almost identical to that of QUADPROG
 %   from MathWorks' Optimization Toolbox. The main difference is that
-%   the linear constraints are specified with A, l, u instead of
-%   A, b, Aeq, beq.
+%   the linear constraints are specified with A, L, U instead of
+%   A, B, Aeq, Beq.
 %
-%   See also MIPS
+%   Calling syntax options:
+%       [x, f, exitflag, output, lambda] = ...
+%           qps_mips(H, c, A, l, u, xmin, xmax, x0, opt)
+%
+%       x = qps_mips(H, c, A, l, u)
+%       x = qps_mips(H, c, A, l, u, xmin, xmax)
+%       x = qps_mips(H, c, A, l, u, xmin, xmax, x0)
+%       x = qps_mips(H, c, A, l, u, xmin, xmax, x0, opt)
+%       x = qps_mips(problem), where problem is a struct with fields:
+%                       H, c, A, l, u, xmin, xmax, x0, opt
+%                       all fields except 'H', 'c', 'A' and 'l' are optional
+%       x = qps_mips(...)
+%       [x, f] = qps_mips(...)
+%       [x, f, exitflag] = qps_mips(...)
+%       [x, f, exitflag, output] = qps_mips(...)
+%       [x, f, exitflag, output, lambda] = qps_mips(...)
+%
+%   Example: (problem from from http://www.uc.edu/sashtml/iml/chap8/sect12.htm)
+%       H = [   1003.1  4.3     6.3     5.9;
+%               4.3     2.2     2.1     3.9;
+%               6.3     2.1     3.5     4.8;
+%               5.9     3.9     4.8     10  ];
+%       c = zeros(4,1);
+%       A = [   1       1       1       1;
+%               0.17    0.11    0.10    0.18    ];
+%       l = [1; 0.10];
+%       u = [1; Inf];
+%       xmin = zeros(4,1);
+%       x0 = [1; 0; 0; 1];
+%       opt = struct('verbose', 2);
+%       [x, f, s, out, lam] = qps_matpower(H, c, A, l, u, xmin, [], x0, opt);
+%
+%   See also MIPS.
 
-%   MATPOWER
+%   MIPS
 %   $Id$
 %   by Ray Zimmerman, PSERC Cornell
 %   Copyright (c) 2010 by Power System Engineering Research Center (PSERC)
