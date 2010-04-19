4e0961a33a83858087edff1eb7dcb74876726366
Ray Zimmerman <rz10@cornell.edu>
2010-04-19 14:41:13 +0000

    Add Contents.m.

---
+++ lib/Contents.m
@@ -0,0 +1,36 @@
+%   MATPOWER
+%   Version 4.0b3      19-Apr-2010 
+%
+%   MATPOWER is a package of Matlab M-files for solving power flow and
+%   optimal power flow problems. It is intended as a simulation tool for
+%   researchers and educators that is easy to use and modify. MATPOWER is
+%   designed to give the best performance possible while keeping the code
+%   simple to understand and modify. It was initially developed as part
+%   of the PowerWeb project <http://www.pserc.cornell.edu/powerweb/>.
+%
+%   MATPOWER can be downloaded from the MATPOWER home page:
+%
+%       http://www.pserc.cornell.edu/matpower/
+%
+%   MATPOWER is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published
+%   by the Free Software Foundation, either version 3 of the License,
+%   or (at your option) any later version.
+%
+%   MATPOWER is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with MATPOWER. If not, see <http://www.gnu.org/licenses/>.
+%
+%   Additional permission under GNU GPL version 3 section 7
+%
+%   If you modify MATPOWER, or any covered work, to interface with
+%   other modules (such as M-files and MEX-files) available in a
+%   Matlab (or compatible) environment containing parts covered
+%   under other licensing terms, the licensors of MATPOWER grant
+%   you additional permission to convey the resulting work.
+%
+%   $Id$
