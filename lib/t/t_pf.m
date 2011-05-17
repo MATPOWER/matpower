eb5fbf861b661169e0f3842dbad5b0bfe2e1a317
Ray Zimmerman <rz10@cornell.edu>
2011-05-17 15:42:07 +0000

    Added tests for network with islands (and multiple gens at slack buses).

--- lib/t/t_pf.m
+++ lib/t/t_pf.m
@@ -34,7 +34,7 @@ if nargin < 1
     quiet = 0;
 end
 
-t_begin(25, quiet);
+t_begin(33, quiet);
 
 casefile = 't_case9_pf';
 if quiet
@@ -48,6 +48,12 @@ if have_fcn('octave')
 end
 mpopt = mpoption('OUT_ALL', 0, 'VERBOSE', verbose);
 
+%% define named indices into bus, gen, branch matrices
+[PQ, PV, REF, NONE, BUS_I, BUS_TYPE, PD, QD, GS, BS, BUS_AREA, VM, ...
+    VA, BASE_KV, ZONE, VMAX, VMIN, LAM_P, LAM_Q, MU_VMAX, MU_VMIN] = idx_bus;
+[F_BUS, T_BUS, BR_R, BR_X, BR_B, RATE_A, RATE_B, RATE_C, ...
+    TAP, SHIFT, BR_STATUS, PF, QF, PT, QT, MU_SF, MU_ST, ...
+    ANGMIN, ANGMAX, MU_ANGMIN, MU_ANGMAX] = idx_brch;
 [GEN_BUS, PG, QG, QMAX, QMIN, VG, MBASE, GEN_STATUS, PMAX, PMIN, ...
     MU_PMAX, MU_PMIN, MU_QMAX, MU_QMIN, PC1, PC2, QC1MIN, QC1MAX, ...
     QC2MIN, QC2MAX, RAMP_AGC, RAMP_10, RAMP_30, RAMP_Q, APF] = idx_gen;
@@ -130,6 +136,41 @@ mpc.gen(1, [QMIN QMAX]) = [-50 0];
 mpc.gen(2, [QMIN QMAX]) = [50 150];
 [baseMVA, bus, gen, branch, success, et] = runpf(mpc, mpopt);
 t_is(gen(1:2, QG), [-50+8.02; 50+16.05], 2, [t '2 gens, proportional']);
+
+%% network with islands
+t = 'network w/islands : DC PF : ';
+mpc0 = loadcase(casefile);
+mpc0.gen(1, PG) = 60;
+mpc0.gen(1, [PMIN PMAX QMIN QMAX PG QG]) = mpc0.gen(1, [PMIN PMAX QMIN QMAX PG QG]) / 2;
+mpc0.gen = [mpc0.gen(1, :); mpc0.gen];
+mpc1 = mpc0;
+mpc  = mpc0;
+nb = size(mpc.bus, 1);
+mpc1.bus(:, BUS_I)		= mpc1.bus(:, BUS_I) + nb;
+mpc1.branch(:, F_BUS)	= mpc1.branch(:, F_BUS) + nb;
+mpc1.branch(:, T_BUS)	= mpc1.branch(:, T_BUS) + nb;
+mpc1.gen(:, GEN_BUS)	= mpc1.gen(:, GEN_BUS) + nb;
+mpc.bus			= [mpc.bus; mpc1.bus];
+mpc.branch		= [mpc.branch; mpc1.branch];
+mpc.gen			= [mpc.gen; mpc1.gen];
+%mpopt = mpoption(mpopt, 'OUT_BUS', 1, 'OUT_GEN', 1, 'OUT_ALL', -1, 'VERBOSE', 2);
+mpopt = mpoption(mpopt, 'VERBOSE', verbose);
+r = rundcpf(mpc, mpopt);
+t_is(r.bus( 1:9,  VA), bus_soln(:, VA), 8, [t 'voltage angles 1']);
+t_is(r.bus(10:18, VA), bus_soln(:, VA), 8, [t 'voltage angles 2']);
+Pg = [gen_soln(1, PG)-30; 30; gen_soln(2:3, PG)];
+t_is(r.gen(1:4, PG), Pg, 8, [t 'active power generation 1']);
+t_is(r.gen(5:8, PG), Pg, 8, [t 'active power generation 1']);
+
+t = 'network w/islands : AC PF : ';
+%% get solved AC power flow case from MAT-file
+load soln9_pf;      %% defines bus_soln, gen_soln, branch_soln
+r = runpf(mpc, mpopt);
+t_is(r.bus( 1:9,  VA), bus_soln(:, VA), 8, [t 'voltage angles 1']);
+t_is(r.bus(10:18, VA), bus_soln(:, VA), 8, [t 'voltage angles 2']);
+Pg = [gen_soln(1, PG)-30; 30; gen_soln(2:3, PG)];
+t_is(r.gen(1:4, PG), Pg, 8, [t 'active power generation 1']);
+t_is(r.gen(5:8, PG), Pg, 8, [t 'active power generation 1']);
 
 if have_fcn('octave')
     warning(s1.state, 'Octave:load-file-in-path');
