40dc0f5aecf613590740d15128238d364454831a
Ray Zimmerman <rz10@cornell.edu>
2012-01-24 14:44:02 +0000

    Added option to makeJac() to return full Jacobian instead of reduced version used in Newton power flow updates.

--- lib/makeJac.m
+++ lib/makeJac.m
@@ -1,13 +1,18 @@
-function [J, Ybus, Yf, Yt] = makeJac(baseMVA, bus, branch, gen)
+function [J, Ybus, Yf, Yt] = makeJac(baseMVA, bus, branch, gen, fullJac)
 %MAKEJAC  Forms the power flow Jacobian.
 %   J = MAKEJAC(MPC)
+%   J = MAKEJAC(MPC, FULLJAC)
 %   J = MAKEJAC(BASEMVA, BUS, BRANCH, GEN)
+%   J = MAKEJAC(BASEMVA, BUS, BRANCH, GEN, FULLJAC)
 %   [J, YBUS, YF, YT] = MAKEJAC(MPC)
 %
 %   Returns the power flow Jacobian and, optionally, the system admittance
 %   matrices. Inputs can be a MATPOWER case struct or individual BASEMVA,
 %   BUS, BRANCH and GEN values. Bus numbers must be consecutive beginning
-%   at 1 (internal ordering).
+%   at 1 (internal ordering). If the FULLJAC argument is present and true,
+%   it returns the full Jacobian (sensitivities of all bus injections
+%   w.r.t all voltage angles/magnitudes) as opposed to the reduced version
+%   used in the Newton power flow updates.
 %
 %   Note: This function builds the Jacobian from scratch, rebuilding the
 %         YBUS matrix in the process. You probably don't want to use this
@@ -46,10 +51,17 @@ function [J, Ybus, Yf, Yt] = makeJac(baseMVA, bus, branch, gen)
 
 if nargin < 4
     mpc     = baseMVA;
+    if nargin > 1
+        fullJac = bus;
+    else
+        fullJac = 0;
+    end
     baseMVA = mpc.baseMVA;
     bus     = mpc.bus;
     branch  = mpc.branch;
     gen     = mpc.gen;
+elseif nargin < 5
+    fullJac = 0;
 end
 
 %% define named indices into bus, gen, branch matrices
@@ -58,9 +70,6 @@ end
 [GEN_BUS, PG, QG, QMAX, QMIN, VG, MBASE, GEN_STATUS, PMAX, PMIN, ...
     MU_PMAX, MU_PMIN, MU_QMAX, MU_QMIN, PC1, PC2, QC1MIN, QC1MAX, ...
     QC2MIN, QC2MAX, RAMP_AGC, RAMP_10, RAMP_30, RAMP_Q, APF] = idx_gen;
-
-%% get bus index lists of each type of bus
-[ref, pv, pq] = bustypes(bus, gen);
 
 %% build Ybus
 [Ybus, Yf, Yt] = makeYbus(baseMVA, bus, branch);
@@ -73,10 +82,20 @@ V(gbus) = gen(on, VG) ./ abs(V(gbus)).* V(gbus);
 
 %% build Jacobian
 [dSbus_dVm, dSbus_dVa] = dSbus_dV(Ybus, V);
-j11 = real(dSbus_dVa([pv; pq], [pv; pq]));
-j12 = real(dSbus_dVm([pv; pq], pq));
-j21 = imag(dSbus_dVa(pq, [pv; pq]));
-j22 = imag(dSbus_dVm(pq, pq));
+if fullJac
+    j11 = real(dSbus_dVa);
+    j12 = real(dSbus_dVm);
+    j21 = imag(dSbus_dVa);
+    j22 = imag(dSbus_dVm);
+else
+    %% get bus index lists of each type of bus
+    [ref, pv, pq] = bustypes(bus, gen);
+
+    j11 = real(dSbus_dVa([pv; pq], [pv; pq]));
+    j12 = real(dSbus_dVm([pv; pq], pq));
+    j21 = imag(dSbus_dVa(pq, [pv; pq]));
+    j22 = imag(dSbus_dVm(pq, pq));
+end
 
 J = [   j11 j12;
         j21 j22;    ];
