31c2352bcfa6595233b1947436541b88136b3488
Ray Zimmerman <rz10@cornell.edu>
2014-03-28 15:55:34 +0000

    Shorten help lines to avoid wrapping.

--- lib/idx_dcline.m
+++ lib/idx_dcline.m
@@ -13,34 +13,34 @@ function c = idx_dcline
 %   below:
 %
 %   columns 1-17 must be included in input matrix (in case file)
-%    1  F_BUS       f, "from" bus number
-%    2  T_BUS       t,  "to"  bus number
-%    3  BR_STATUS   initial branch status, 1 - in service, 0 - out of service
-%    4  PF          MW flow at "from" bus ("from" -> "to")
-%    5  PT          MW flow at  "to"  bus ("from" -> "to")
-%    6  QF          MVAr injection at "from" bus ("from" -> "to")
-%    7  QT          MVAr injection at  "to"  bus ("from" -> "to")
-%    8  VF          voltage setpoint at "from" bus (p.u.)
-%    9  VT          voltage setpoint at  "to"  bus (p.u.)
-%   10  PMIN        lower limit on PF (MW flow at "from" end)
-%   11  PMAX        upper limit on PF (MW flow at "from" end)
-%   12  QMINF       lower limit on MVAr injection at "from" bus
-%   13  QMAXF       upper limit on MVAr injection at "from" bus
-%   14  QMINT       lower limit on MVAr injection at  "to"  bus
-%   15  QMAXT       upper limit on MVAr injection at  "to"  bus
-%   16  LOSS0       constant term of linear loss function (MW)
-%   17  LOSS1       linear term of linear loss function (MW/MW)
-%                   (loss = LOSS0 + LOSS1 * PF)
+%    1  F_BUS     f, "from" bus number
+%    2  T_BUS     t,  "to"  bus number
+%    3  BR_STATUS initial branch status, 1 - in service, 0 - out of service
+%    4  PF        MW flow at "from" bus ("from" -> "to")
+%    5  PT        MW flow at  "to"  bus ("from" -> "to")
+%    6  QF        MVAr injection at "from" bus ("from" -> "to")
+%    7  QT        MVAr injection at  "to"  bus ("from" -> "to")
+%    8  VF        voltage setpoint at "from" bus (p.u.)
+%    9  VT        voltage setpoint at  "to"  bus (p.u.)
+%   10  PMIN      lower limit on PF (MW flow at "from" end)
+%   11  PMAX      upper limit on PF (MW flow at "from" end)
+%   12  QMINF     lower limit on MVAr injection at "from" bus
+%   13  QMAXF     upper limit on MVAr injection at "from" bus
+%   14  QMINT     lower limit on MVAr injection at  "to"  bus
+%   15  QMAXT     upper limit on MVAr injection at  "to"  bus
+%   16  LOSS0     constant term of linear loss function (MW)
+%   17  LOSS1     linear term of linear loss function (MW/MW)
+%                 (loss = LOSS0 + LOSS1 * PF)
 %
 %   columns 18-23 are added to matrix after OPF solution
 %   they are typically not present in the input matrix
-%                   (assume OPF objective function has units, u)
-%   18  MU_PMIN     Kuhn-Tucker multiplier on lower flow lim at "from" bus (u/MW)
-%   19  MU_PMAX     Kuhn-Tucker multiplier on upper flow lim at "from" bus (u/MW)
-%   20  MU_QMINF    Kuhn-Tucker multiplier on lower VAr lim at "from" bus (u/MVAr)
-%   21  MU_QMAXF    Kuhn-Tucker multiplier on upper VAr lim at "from" bus (u/MVAr)
-%   22  MU_QMINT    Kuhn-Tucker multiplier on lower VAr lim at  "to"  bus (u/MVAr)
-%   23  MU_QMAXT    Kuhn-Tucker multiplier on upper VAr lim at  "to"  bus (u/MVAr)
+%                 (assume OPF objective function has units, u)
+%   18  MU_PMIN   Kuhn-Tucker multiplier on lower flow lim at "from" bus (u/MW)
+%   19  MU_PMAX   Kuhn-Tucker multiplier on upper flow lim at "from" bus (u/MW)
+%   20  MU_QMINF  Kuhn-Tucker multiplier on lower VAr lim at "from" bus (u/MVAr)
+%   21  MU_QMAXF  Kuhn-Tucker multiplier on upper VAr lim at "from" bus (u/MVAr)
+%   22  MU_QMINT  Kuhn-Tucker multiplier on lower VAr lim at  "to"  bus (u/MVAr)
+%   23  MU_QMAXT  Kuhn-Tucker multiplier on upper VAr lim at  "to"  bus (u/MVAr)
 %
 %   See also TOGGLE_DCLINE.
 
