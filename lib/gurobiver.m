913fae4a0e1010f0b1f1d383cc0e1fd278c37e8a
Ray Zimmerman <rz10@cornell.edu>
2014-06-17 19:57:18 +0000

    Changed MIPS to MATPOWER in license text.

--- lib/gurobiver.m
+++ lib/gurobiver.m
@@ -13,28 +13,28 @@ function rv = gurobiver(varargin)
 %   by Ray Zimmerman, PSERC Cornell
 %   Copyright (c) 2010, 2012 by Power System Engineering Research Center (PSERC)
 %
-%   This file is part of MIPS.
+%   This file is part of MATPOWER.
 %   See http://www.pserc.cornell.edu/matpower/ for more info.
 %
-%   MIPS is free software: you can redistribute it and/or modify
+%   MATPOWER is free software: you can redistribute it and/or modify
 %   it under the terms of the GNU General Public License as published
 %   by the Free Software Foundation, either version 3 of the License,
 %   or (at your option) any later version.
 %
-%   MIPS is distributed in the hope that it will be useful,
+%   MATPOWER is distributed in the hope that it will be useful,
 %   but WITHOUT ANY WARRANTY; without even the implied warranty of
 %   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 %   GNU General Public License for more details.
 %
 %   You should have received a copy of the GNU General Public License
-%   along with MIPS. If not, see <http://www.gnu.org/licenses/>.
+%   along with MATPOWER. If not, see <http://www.gnu.org/licenses/>.
 %
 %   Additional permission under GNU GPL version 3 section 7
 %
-%   If you modify MIPS, or any covered work, to interface with
+%   If you modify MATPOWER, or any covered work, to interface with
 %   other modules (such as MATLAB code and MEX-files) available in a
 %   MATLAB(R) or comparable environment containing parts covered
-%   under other licensing terms, the licensors of MIPS grant
+%   under other licensing terms, the licensors of MATPOWER grant
 %   you additional permission to convey the resulting work.
 
 try
