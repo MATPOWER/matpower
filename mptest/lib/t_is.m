82cf7887b4bc39a8fe2cc37c87e18afbea6067f1
Ray Zimmerman <rz10@cornell.edu>
2014-10-11 15:31:10 +0000

    Added handling of NaN values to t_is().

--- mptest/lib/t_is.m
+++ mptest/lib/t_is.m
@@ -9,7 +9,8 @@ function ok = t_is(got, expected, prec, msg)
 %   T_BEGIN and T_END.
 %
 %   Optionally returns a true or false value indicating whether or
-%   not the test succeeded.
+%   not the test succeeded. NaN's are considered to be equal to each
+%   other.
 %
 %   Example:
 %       quiet = 0;
@@ -63,9 +64,23 @@ if all(size(got) == [m, n]) || all([m, n] == [1 1])
     if m == 0 || n == 0
         condition = true;
     else
-        got_minus_expected = got - expected;
-        max_diff = max(max(abs(got_minus_expected)));
-        condition = ( max_diff < 10^(-prec) );
+        %% check for NaNs!
+        gNaN = find(isnan(got(:)));
+        eNaN = find(isnan(expected(:)));
+        if ~isscalar(expected) && ...
+                (length(gNaN) ~= length(eNaN) || sum(gNaN-eNaN) ~= 0) || ...
+            isscalar(expected) && ...
+                    (( isnan(expected) && ~all(isnan(got))) || ...
+                     (~isnan(expected) && any(isnan(got))) )
+            condition = false;
+            max_diff = -1;
+        elseif all(all(isnan(got))) && all(all(isnan(got)))
+            condition = true;
+        else
+            got_minus_expected = got - expected;
+            max_diff = max(max(abs(got_minus_expected)));
+            condition = ( max_diff < 10^(-prec) );
+        end
     end
 else
     condition = false;
@@ -74,21 +89,28 @@ end
 
 t_ok(condition, msg);
 if ~condition && ~t_quiet
-    if max_diff ~= 0
+    if max_diff > 0
         [i, j, v] = find(~(abs(got_minus_expected) < 10^(-prec)));
         k = i+(j-1)*m;
         [vv, kk] = max(abs(got_minus_expected(k)));
         fprintf('  row     col          got             expected          got - exp\n');
         fprintf('-------  ------  ----------------  ----------------  ----------------');
         for u = 1:length(i)
+            if isscalar(expected)
+                ex = expected;
+            else
+                ex = expected(k(u));
+            end
             fprintf('\n%6d  %6d  %16g  %16g  %16g', ...
-                [i(u) j(u) got(k(u)) expected(k(u)) got_minus_expected(k(u))]');
+                [i(u) j(u) got(k(u)) ex got_minus_expected(k(u))]');
             if u == kk
                 fprintf('  *');
             end
         end
         fprintf('\nmax diff @ (%d,%d) = %g > allowed tol of %g\n\n', ...
             i(kk), j(kk), max_diff, 10^(-prec));
+    elseif max_diff == -1
+        fprintf('    mismatch in locations of NaNs\n');
     else
         fprintf('    dimension mismatch:\n');
         fprintf('             got: %d x %d\n', size(got));
