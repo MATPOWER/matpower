e6d3aeb082a51a69e96a19ad68d2235f5929329e
Ray Zimmerman <rz10@cornell.edu>
2015-01-21 18:50:17 +0000

    Fixed minor bug with poly2pwl(), affecting units with Pmax <= 0.

--- lib/poly2pwl.m
+++ lib/poly2pwl.m
@@ -2,14 +2,14 @@ function pwlcost = poly2pwl(polycost, Pmin, Pmax, npts)
 %POLY2PWL  Converts polynomial cost variable to piecewise linear.
 %   PWLCOST = POLY2PWL(POLYCOST, PMIN, PMAX, NPTS) converts the polynomial
 %   cost variable POLYCOST into a piece-wise linear cost by evaluating at
-%   zero and then at NPTS evenly spaced points between PMIN and PMAX. If
-%   PMIN <= 0 (such as for reactive power, where P really means Q) it just
-%   uses NPTS evenly spaced points between PMIN and PMAX.
+%   NPTS evenly spaced points between PMIN and PMAX. If the range does not
+%   include 0, then it is evaluated at 0 and NPTS-1 evenly spaced points
+%   between PMIN and PMAX.
 
 %   MATPOWER
 %   $Id$
 %   by Ray Zimmerman, PSERC Cornell
-%   Copyright (c) 1996-2010 by Power System Engineering Research Center (PSERC)
+%   Copyright (c) 1996-2015 by Power System Engineering Research Center (PSERC)
 %
 %   This file is part of MATPOWER.
 %   See http://www.pserc.cornell.edu/matpower/ for more info.
@@ -38,19 +38,20 @@ function pwlcost = poly2pwl(polycost, Pmin, Pmax, npts)
 [PW_LINEAR, POLYNOMIAL, MODEL, STARTUP, SHUTDOWN, NCOST, COST] = idx_cost;
 
 pwlcost = polycost;
-[m, n] = size(polycost);                                %% size of piece being changed
-pwlcost(:, MODEL)  = PW_LINEAR * ones(m, 1);            %% change cost model
-pwlcost(:, COST:n) = zeros(size(pwlcost(:, COST:n)));   %% zero out old data
-pwlcost(:, NCOST)  = npts * ones(m, 1);                 %% change number of data points
+[m, n] = size(polycost);            %% size of piece being changed
+pwlcost(:, MODEL)  = PW_LINEAR;     %% change cost model
+pwlcost(:, COST:n) = 0;             %% zero out old data
+pwlcost(:, NCOST)  = npts;          %% change number of data points
+pwlcost(1, COST+2*(npts-1)+1) = 0;  %% expand as needed
 
 for i = 1:m
-    if Pmin(i) == 0
-        step = (Pmax(i) - Pmin(i)) / (npts - 1);
-        xx = (Pmin(i):step:Pmax(i));
-    elseif Pmin(i) > 0
+    if Pmin(i) > 0
         step = (Pmax(i) - Pmin(i)) / (npts - 2);
         xx = [0 Pmin(i):step:Pmax(i)];
-    elseif Pmin(i) < 0 && Pmax(i) > 0        %% for when P really means Q
+    elseif Pmax(i) < 0
+        step = (Pmax(i) - Pmin(i)) / (npts - 2);
+        xx = [Pmin(i):step:Pmax(i) 0];
+    else
         step = (Pmax(i) - Pmin(i)) / (npts - 1);
         xx = (Pmin(i):step:Pmax(i));
     end
