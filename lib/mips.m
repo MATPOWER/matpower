e65eda61de1f5e1db1310b50ecfa357807d0e8d5
Ray Zimmerman <rz10@cornell.edu>
2015-02-24 18:41:31 +0000

    Add check on empty A matrix (for consistency across wrappers).

--- lib/mips.m
+++ lib/mips.m
@@ -240,8 +240,8 @@ else                                    %% individual args
     end
 end
 %% set default argument values if missing
-if  ~isempty(A) && (isempty(l) || all(l == -Inf)) && ...
-                   (isempty(u) || all(u == Inf))
+if isempty(A) || ~isempty(A) && (isempty(l) || all(l == -Inf)) && ...
+                                (isempty(u) || all(u == Inf))
     A = sparse(0,nx);           %% no limits => no linear constraints
 end
 nA = size(A, 1);                %% number of original linear constraints
