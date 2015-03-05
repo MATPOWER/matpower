b031dda07b59246bc5a1c734db547108b83d8448
Ray Zimmerman <rz10@cornell.edu>
2015-03-05 19:26:48 +0000

    Copyright updates.

--- lib/t/t_mips.m
+++ lib/t/t_mips.m
@@ -2,9 +2,10 @@ function t_mips(quiet)
 %T_MIPS  Tests of MIPS NLP solver.
 
 %   MIPS
-%   $Id$
+%   Copyright (c) 2010-2015 by Power System Engineering Research Center (PSERC)
 %   by Ray Zimmerman, PSERC Cornell
-%   Copyright (c) 2010 by Power System Engineering Research Center (PSERC)
+%
+%   $Id$
 %
 %   This file is part of MIPS.
 %   Covered by the 3-clause BSD License (see LICENSE file for details).
