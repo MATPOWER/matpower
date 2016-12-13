231b118a8f0f8d49c887c1610e46841ce2443ea2
Ray Zimmerman <rz10@cornell.edu>
2016-12-13 15:32:58 -0500

    PSERC is “Systems” not “System”.

--- lib/mipsver.m
+++ lib/mipsver.m
@@ -9,7 +9,7 @@ function rv = mipsver(varargin)
 %   See also MPVER.
 
 %   MIPS
-%   Copyright (c) 2010-2016 by Power System Engineering Research Center (PSERC)
+%   Copyright (c) 2010-2016, Power Systems Engineering Research Center (PSERC)
 %   by Ray Zimmerman, PSERC Cornell
 %
 %   This file is part of MIPS.
