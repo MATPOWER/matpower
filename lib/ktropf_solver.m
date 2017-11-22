820730e934a80a094aa58c7eb43d7e1beac30e36
Ray Zimmerman <rz10@cornell.edu>
2017-11-22 14:47:03 -0500

    Adjust interior starting point closer to bounds.
    ... for variables bounded on one side.

--- lib/ktropf_solver.m
+++ lib/ktropf_solver.m
@@ -95,14 +95,15 @@ bfeq = u(ieq);
 
 %% try to select an interior initial point
 if mpopt.opf.init_from_mpc ~= 1
+    s = 1e-3;                   %% set init point inside bounds by s
     ll = xmin; uu = xmax;
     ll(xmin == -Inf) = -1e10;   %% replace Inf with numerical proxies
     uu(xmax ==  Inf) =  1e10;
     x0 = (ll + uu) / 2;         %% set x0 mid-way between bounds
     k = find(xmin == -Inf & xmax < Inf);    %% if only bounded above
-    x0(k) = xmax(k) - 1;                    %% set just below upper bound
+    x0(k) = xmax(k) - s;                    %% set just below upper bound
     k = find(xmin > -Inf & xmax == Inf);    %% if only bounded below
-    x0(k) = xmin(k) + 1;                    %% set just above lower bound
+    x0(k) = xmin(k) + s;                    %% set just above lower bound
     Varefs = bus(bus(:, BUS_TYPE) == REF, VA) * (pi/180);
     x0(vv.i1.Va:vv.iN.Va) = Varefs(1);  %% angles set to first reference angle
     if ny > 0
