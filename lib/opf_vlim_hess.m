ab52da6bb0fac5f068f0b9cbb02504106be6ef8a
Baljinnyam Sereeter <b.sereeter@tudelft.nl>
2018-03-12 14:20:45 +0100

    change voltage magnitude to voltage magnitude violations

--- lib/opf_vlim_hess.m
+++ lib/opf_vlim_hess.m
@@ -1,6 +1,6 @@
-function d2Vm = opf_vlim_hess(x, lam, ref, mpopt)
+function d2Vlims = opf_vlim_hess(x, lambda, mpc, mpopt)
 %OPF_VLIM_HESS  Evaluates Hessian of voltage magnitudes.
-%   D2VM = OPF_VLIM_HESS(X, LAMBDA, REF, MPOPT)
+%   D2VLIMS = OPF_VLIM_HESS(X, LAMBDA, REF, MPOPT)
 %
 %   Hessian evaluation function for voltage magnitudes.
 %
@@ -11,10 +11,10 @@ function d2Vm = opf_vlim_hess(x, lam, ref, mpopt)
 %     MPOPT : MATPOWER options struct
 %
 %   Outputs:
-%     D2VM : Hessian of voltage magnitudes.
+%     D2VLIMS : Hessian of voltage magnitudes.
 %
 %   Example:
-%       d2VM = opf_vlim_hess(x, lambda, ref, mpopt);
+%       d2Vlims = opf_vlim_hess(x, lambda, ref, mpopt);
 %
 %   See also OPF_VLIM_FCN.
 
@@ -34,23 +34,39 @@ nb = length(Vi);            %% number of buses
 
 %% compute voltage magnitude
 Vm = sqrt(Vr.^2 + Vi.^2);
-
-% nlam = length(lambda) / 2;
-% lamP = lambda(1:nlam);
-% lamQ = lambda((1:nlam)+nlam);
+      
+%% ----- evaluate constraint gradients -----
+nlam = length(lambda) / 2;
+if nlam
+    lamUp = lambda(1:nlam);
+    lamLow = lambda((1:nlam)+nlam);
+else
+    lamUp = zeros(0,1);   %% (required to avoid problems when using Knitro
+    lamLow = zeros(0,1);   %%  on cases with all lines unconstrained)    
+end
 
 %% ----- evaluate constraint gradients -----
 diagVi       = sparse(1:n, 1:n, Vi, n, n);    
 diagVr       = sparse(1:n, 1:n, Vr, n, n);    
-diagVm1lam   = sparse(1:n, 1:n, lam./Vm, n, n);    
-diagVilamVm3 = sparse(1:n, 1:n, (Vi.*lam)./(Vm.^3), n, n);    
-diagVrlamVm3 = sparse(1:n, 1:n, (Vr.*lam)./(Vm.^3), n, n);    
+%% for upper limit
+    diagVm1lam   = sparse(1:n, 1:n, lamUp./Vm, n, n);    
+    diagVilamVm3 = sparse(1:n, 1:n, (Vi.*lamUp)./(Vm.^3), n, n);    
+    diagVrlamVm3 = sparse(1:n, 1:n, (Vr.*lamUp)./(Vm.^3), n, n);    
 
-Vm_ii = diagVm1lam - diagVi*diagVilamVm3;
-Vm_ir = - diagVi*diagVrlamVm3 ;
-Vm_ri = Vm_ir;
-Vm_rr = diagVm1lam - diagVr*diagVrlamVm3;
+    VlimU_ii = diagVm1lam - diagVi*diagVilamVm3;
+    VlimU_ir = - diagVi*diagVrlamVm3 ;
+    VlimU_ri = Vm_ir;
+    VlimU_rr = diagVm1lam - diagVr*diagVrlamVm3;
+%% for lower limit
+    diagVm1lam   = sparse(1:n, 1:n, lamLow./Vm, n, n);    
+    diagVilamVm3 = sparse(1:n, 1:n, (Vi.*lamLow)./(Vm.^3), n, n);    
+    diagVrlamVm3 = sparse(1:n, 1:n, (Vr.*lamLow)./(Vm.^3), n, n);    
+
+    VlimL_ii = diagVm1lam - diagVi*diagVilamVm3;
+    VlimL_ir = - diagVi*diagVrlamVm3 ;
+    VlimL_ri = Vm_ir;
+    VlimL_rr = diagVm1lam - diagVr*diagVrlamVm3;    
 %% construct Hessian
-d2Vm = [ Vm_ii Vm_ir; Vm_ri Vm_rr];    
+d2Vlims = -[ VlimU_ii VlimU_ir; VlimU_ri VlimU_rr] + [ VlimL_ii VlimL_ir; VlimL_ri VlimL_rr];     
 
 end
