7fc29bbdd08d7e3c950df87d4cb238a36b8d7bb6
Baljinnyam Sereeter <b.sereeter@tudelft.nl>
2018-03-16 18:25:56 +0100

    create a function computing branch angle difference

---
+++ lib/opf_branch_ang_fcn.m
@@ -0,0 +1,23 @@
+function [VaDif, dVaDif] = opf_branch_ang_fcn(x, Aang, lang, uang, iang, mpopt);
+
+%% unpack data
+[Vr, Vi] = deal(x{:});
+
+%% problem dimensions
+nb = length(Vi);            %% number of buses
+
+%% compute branch angle difference
+Va = angle(Vr + 1j* Vi);
+Ax = Aang * Va;
+VaDif = [ lang - Ax;
+             Ax - uang ];
+               
+if nargout > 1
+    %% compute partials of branch angle difference w.r.t Vr and Vi
+    Vm2 = Vr.^2 + Vi.^2;
+    AangdVa_dVr = Aang*sparse(1:nb, 1:nb, -Vi./Vm2, nb, nb);
+    AangdVa_dVi = Aang*sparse(1:nb, 1:nb,  Vr./Vm2, nb, nb);
+    dVaDif = [ -[AangdVa_dVr AangdVa_dVi];                      %% VaDif w.r.t Vr, Vi
+                [AangdVa_dVr AangdVa_dVi]];
+end
+end
