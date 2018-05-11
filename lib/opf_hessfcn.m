ae1a60c8cafb792b2231156979708d78fd97ac42
Ray Zimmerman <rz10@cornell.edu>
2018-05-10 21:27:26 -0400

    Update opf_hessfcn() to compute d2f via opf_costfcn().

--- lib/opf_hessfcn.m
+++ lib/opf_hessfcn.m
@@ -40,89 +40,8 @@ function Lxx = opf_hessfcn(x, lambda, cost_mult, om, Ybus, Yf, Yt, mpopt, il)
 %   Covered by the 3-clause BSD License (see LICENSE file for details).
 %   See http://www.pserc.cornell.edu/matpower/ for more info.
 
-%%----- initialize -----
-%% define named indices into data matrices
-[GEN_BUS, PG, QG, QMAX, QMIN, VG, MBASE, GEN_STATUS, PMAX, PMIN, ...
-    MU_PMAX, MU_PMIN, MU_QMAX, MU_QMIN, PC1, PC2, QC1MIN, QC1MAX, ...
-    QC2MIN, QC2MAX, RAMP_AGC, RAMP_10, RAMP_30, RAMP_Q, APF] = idx_gen;
-[F_BUS, T_BUS, BR_R, BR_X, BR_B, RATE_A, RATE_B, RATE_C, ...
-    TAP, SHIFT, BR_STATUS, PF, QF, PT, QT, MU_SF, MU_ST, ...
-    ANGMIN, ANGMAX, MU_ANGMIN, MU_ANGMAX] = idx_brch;
-[PW_LINEAR, POLYNOMIAL, MODEL, STARTUP, SHUTDOWN, NCOST, COST] = idx_cost;
-
-%% default args
-if isempty(cost_mult)
-    cost_mult = 1;
-end
-
-%% unpack data
-mpc = om.get_mpc();
-[baseMVA, gen, branch, gencost] = ...
-    deal(mpc.baseMVA, mpc.gen, mpc.branch, mpc.gencost);
-cp = om.get_cost_params();
-[N, Cw, H, dd, rh, kk, mm] = deal(cp.N, cp.Cw, cp.H, cp.dd, ...
-                                    cp.rh, cp.kk, cp.mm);
-vv = om.get_idx();
-
-%% unpack needed parameters
-nl = size(branch, 1);       %% number of branches
-ng = size(gen, 1);          %% number of dispatchable injections
-nxyz = length(x);           %% total number of control vars of all types
-
-%% set default constrained lines
-if nargin < 8
-    il = (1:nl);            %% all lines have limits by default
-end
-
-%% grab Pg & Qg
-Pg = x(vv.i1.Pg:vv.iN.Pg);  %% active generation in p.u.
-Qg = x(vv.i1.Qg:vv.iN.Qg);  %% reactive generation in p.u.
-
-%% reconstruct V
-pcost = gencost(1:ng, :);
-if size(gencost, 1) > ng
-    qcost = gencost(ng+1:2*ng, :);
-else
-    qcost = [];
-end
-
 %% ----- evaluate d2f -----
-d2f_dPg2 = sparse(ng, 1);               %% w.r.t. p.u. Pg
-d2f_dQg2 = sparse(ng, 1);               %% w.r.t. p.u. Qg
-ipolp = find(pcost(:, MODEL) == POLYNOMIAL);
-d2f_dPg2(ipolp) = baseMVA^2 * polycost(pcost(ipolp, :), Pg(ipolp)*baseMVA, 2);
-if ~isempty(qcost)          %% Qg is not free
-    ipolq = find(qcost(:, MODEL) == POLYNOMIAL);
-    d2f_dQg2(ipolq) = baseMVA^2 * polycost(qcost(ipolq, :), Qg(ipolq)*baseMVA, 2);
-end
-i = [vv.i1.Pg:vv.iN.Pg vv.i1.Qg:vv.iN.Qg]';
-d2f = sparse(i, i, [d2f_dPg2; d2f_dQg2], nxyz, nxyz);
-
-%% generalized cost
-if ~isempty(N)
-    nw = size(N, 1);
-    r = N * x - rh;                 %% Nx - rhat
-    iLT = find(r < -kk);            %% below dead zone
-    iEQ = find(r == 0 & kk == 0);   %% dead zone doesn't exist
-    iGT = find(r > kk);             %% above dead zone
-    iND = [iLT; iEQ; iGT];          %% rows that are Not in the Dead region
-    iL = find(dd == 1);             %% rows using linear function
-    iQ = find(dd == 2);             %% rows using quadratic function
-    LL = sparse(iL, iL, 1, nw, nw);
-    QQ = sparse(iQ, iQ, 1, nw, nw);
-    kbar = sparse(iND, iND, [   ones(length(iLT), 1);
-                                zeros(length(iEQ), 1);
-                                -ones(length(iGT), 1)], nw, nw) * kk;
-    rr = r + kbar;                  %% apply non-dead zone shift
-    M = sparse(iND, iND, mm(iND), nw, nw);  %% dead zone or scale
-    diagrr = sparse(1:nw, 1:nw, rr, nw, nw);
-    
-    %% linear rows multiplied by rr(i), quadratic rows by rr(i)^2
-    w = M * (LL + QQ * diagrr) * rr;
-    HwC = H * w + Cw;
-    AA = N' * M * (LL + 2 * QQ * diagrr);
-    d2f = d2f + AA * H * AA' + 2 * N' * M * QQ * sparse(1:nw, 1:nw, HwC, nw, nw) * N;
-end
+[f, df, d2f] = opf_costfcn(x, om);
 d2f = d2f * cost_mult;
 
 %%----- evaluate Hessian of power balance constraints -----
@@ -133,6 +52,12 @@ d2H = om.eval_nln_constraint_hess(x, lambda.ineqnonlin, 0);
 
 %%-----  do numerical check using (central) finite differences  -----
 if 0
+    mpc = om.get_mpc();
+    nl = size(mpc.branch, 1);       %% number of branches
+    if nargin < 9
+        il = (1:nl);            %% all lines have limits by default
+    end
+
     nx = length(x);
     step = 1e-5;
     num_d2f = sparse(nx, nx);
