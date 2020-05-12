4597a8f7a9f6b1aca49c3b7d911dadca96e08628
Timon Viola <tvi@sbtinstruments.com>
2020-05-12 22:21:24 +0200

    Fix loadcase string input (#100)
    * fix: add string input type support to loadcase
    * fix: add ; to suppress output
    * fix: change casefile type check to ensure backward compatibility
    * fix: update input arg type error message
    
    Ref: https://github.com/MATPOWER/matpower/issues/99
    Ref: https://github.com/MATPOWER/matpower/issues/100

--- lib/loadcase.m
+++ lib/loadcase.m
@@ -50,6 +50,9 @@ else
 end
 
 %%-----  read data into struct  -----
+if isa(casefile, 'string')
+    casefile = char(casefile);
+end
 if ischar(casefile)
     [pathstr, fname, ext] = fileparts(casefile);
     if isempty(ext)
@@ -185,7 +188,7 @@ if info == 0    %% no errors
 else            %% we have a problem captain
     switch info
         case 1,
-            error('loadcase: input arg should be a struct or a string containing a filename');
+            error('loadcase: input arg should be a struct or a char array/string containing a filename');
         case 2,
             error('loadcase: specified case not in MATLAB''s search path');
         case 3,
