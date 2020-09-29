9766ecf63d9ac49bcc615951ed022305c4f58a6f
Ray Zimmerman <rz10@cornell.edu>
2020-09-29 16:58:17 -0500

    Select baseMVA for new distribution system cases based on total load.
    < 10 MW  – use 1 MVA
    > 10 MW  – use 10 MVA

--- data/case136ma.m
+++ data/case136ma.m
@@ -12,7 +12,7 @@ mpc.version = '2';
 
 %%-----  Power Flow Data  -----%%
 %% system MVA base
-mpc.baseMVA = 100;
+mpc.baseMVA = 10;
 
 %% bus data
 %	bus_i	type	Pd	Qd	Gs	Bs	area	Vm	Va	baseKV	zone	Vmax	Vmin
