f937aa181936189eca944a953535e220bd59c3cb
Azuk 443 <me@azuk.top>
2023-05-19 23:34:02 +0800

    Add an option for masking bridge branches in makeLODF.

--- lib/makeLODF.m
+++ lib/makeLODF.m
@@ -1,22 +1,50 @@
-function LODF = makeLODF(branch, PTDF);
+function LODF = makeLODF(branch, PTDF, mask_bridge);
 %MAKELODF   Builds the line outage distribution factor matrix.
-%   LODF = MAKELODF(BRANCH, PTDF) returns the DC line outage
-%   distribution factor matrix for a given PTDF. The matrix is nbr x nbr,
-%   where nbr is the number of branches.
+%   LODF = MAKELODF(MPC, PTDF)
+%   LODF = MAKELODF(BRANCH, PTDF)
+%   LODF = MAKELODF(MPC, PTDF, MASK_BRIDGE)
+% 
+%   Returns the DC line outage distribution factor matrix for a given PTDF.
+%   The matrix is nbr x nbr, where nbr is the number of branches.
 %
 %   Example:
-%       H = makePTDF(baseMVA, bus, branch);
+%       H = makePTDF(mpc);
 %       LODF = makeLODF(branch, H);
+%       LODF = makeLODF(mpc, H);
 %
-%   See also MAKEPTDF.
+%       % mask bridge branches in LODF
+%       makeLODF(mpc, H, 1);
+%
+%   See also MAKEPTDF, FIND_BRIDGES.
 
 %   MATPOWER
 %   Copyright (c) 2008-2016, Power Systems Engineering Research Center (PSERC)
-%   by Ray Zimmerman, PSERC Cornell
+%   by Ray Zimmerman, PSERC Cornell,
+%   and Liangyu Zhang, Zhejiang University
+%
+%   Modified by Liangyu Zhang
+%   2023.05.19 Mask corresponding columns of network bridge branches
+
 %
 %   This file is part of MATPOWER.
 %   Covered by the 3-clause BSD License (see LICENSE file for details).
 %   See https://matpower.org for more info.
+
+if isstruct(branch)
+    mpc = branch;
+    branch = mpc.branch;
+end
+
+if nargin < 3
+    mask_bridge = 0;
+else %% nargin >= 3
+    if ~exist('mpc', 'var')
+        error('MPC is required when mask_bridge is enabled.');
+    end
+end
+if ~exist('mask_bridge', 'var')
+    mask_bridge = 0;
+end
 
 [F_BUS, T_BUS, BR_R, BR_X, BR_B, RATE_A, RATE_B, RATE_C, ...
     TAP, SHIFT, BR_STATUS, PF, QF, PT, QT, MU_SF, MU_ST, ...
@@ -31,3 +59,13 @@ H = PTDF * Cft;
 h = diag(H, 0);
 LODF = H ./ (ones(nl, nl) - ones(nl, 1) * h');
 LODF = LODF - diag(diag(LODF)) - eye(nl, nl);
+
+if mask_bridge ~= 0 
+    [~, bridges, nonbridges] = find_bridges(mpc);
+    for i = 1:length(bridges)
+        br = bridges{i};
+        nonbr = nonbridges{i};
+        LODF(:, br) = NaN;  % mask bridge branch columns
+        LODF(setdiff(1:nb, [br; nonbr;]), nonbr) = NaN; % one island should not affect other islands
+    end
+end
