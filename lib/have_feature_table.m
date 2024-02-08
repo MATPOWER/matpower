db072f649908f59571e3f5b34d4ebaf769c2bf44
Ray Zimmerman <rz10@cornell.edu>
2024-02-08 14:10:38 -0700

    Add to have_feature_table() ability to capture version and date for Tablicious tables in Octave.

--- lib/have_feature_table.m
+++ lib/have_feature_table.m
@@ -3,14 +3,13 @@ function [TorF, vstr, rdate] = have_feature_table()
 %
 %   Feature detection function implementing 'table' tag for HAVE_FEATURE
 %   to detect availability/version of TABLE, included in MATLAB R2013b
-%   and as of this writing in May 2021, available for Octave as Tablicious:
-%       https://tablicious.janklab.net
+%   and as of this writing in Feb 2024, available for Octave as Tablicious:
 %       https://github.com/apjanke/octave-tablicious
 %
 %   See also HAVE_FEATURE, TABLE.
 
 %   MATPOWER
-%   Copyright (c) 2021, Power Systems Engineering Research Center (PSERC)
+%   Copyright (c) 2021-2024, Power Systems Engineering Research Center (PSERC)
 %   by Ray Zimmerman, PSERC Cornell
 %
 %   This file is part of MATPOWER.
@@ -18,10 +17,19 @@ function [TorF, vstr, rdate] = have_feature_table()
 %   See https://matpower.org for more info.
 
 TorF = exist('table', 'file') == 2;
-if TorF && have_feature('matlab')
-    vstr = have_feature('matlab', 'vstr');
-    rdate = have_feature('matlab', 'date');
-else
-    vstr = '';
-    rdate = '';
+vstr = '';
+rdate = '';
+if TorF
+    if have_feature('matlab')
+        vstr = have_feature('matlab', 'vstr');
+        rdate = have_feature('matlab', 'date');
+    else
+        v = ver('tablicious');
+        if ~isempty(v) && isfield(v, 'Version')
+            vstr = v.Version;
+        end
+        if ~isempty(v) && isfield(v, 'Date')
+            rdate = v.Date;
+        end
+    end
 end
