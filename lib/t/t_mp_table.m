ab0d1e808ae082d11b8dbe67bd0281efd0cc0058
Mahnoor Fatima <59219220+maxwell04-wq@users.noreply.github.com>
2024-03-24 00:59:14 +0500

    Update t_mp_table.m
    Line 200 throws an error in MATLAB 2018b
    ```
    Table variable subscripts must be real positive integers, logicals, character vectors, or cell arrays of character vectors.
    ```
    The quotation marks have been made consistent with other commands to make the code run smoothly on MATLAB 2018b.

--- lib/t/t_mp_table.m
+++ lib/t/t_mp_table.m
@@ -197,7 +197,7 @@ for k = 1:nc
     t_ok(isequal(T{:, 2:2:6}, [v2 v4 v6]), [t 'T{:, j1:2:jN}']);
 
     t_ok(isequal(T{:, 'igr'}, v1), [t 'T{:, ''igr''} == v1']);
-    t_ok(isequal(T{:, "flt"}, v2), [t 'T{:, "flt"} == v2']);
+    t_ok(isequal(T{:, 'flt'}, v2), [t 'T{:, ''flt''} == v2']);
     t_ok(isequal(T{:, 'str'}, v3), [t 'T{:, ''str''} == v3']);
     t_ok(isequal(T{:, 'dbl'}, v4), [t 'T{:, ''dbl''} == v4']);
     t_ok(isequal(T{:, 'boo'}, v5), [t 'T{:, ''boo''} == v5']);
