99205847f50feaf7f3c094ac76db30687374a4de
Ray Zimmerman <rz10@cornell.edu>
2025-04-17 10:53:33 -0600

    Add an option to escape '+' in paths in source URLs in generate_auto_doc_stubs().

--- lib/t/generate_autodoc_stubs.m
+++ lib/t/generate_autodoc_stubs.m
@@ -1,8 +1,9 @@
-function [success, err_list] = generate_autodoc_stubs(in, dest)
+function [success, err_list] = generate_autodoc_stubs(in, dest, escape_plus)
 % generate_autodoc_stubs - Generate source .rst stubs for Reference Manual.
 % ::
 %
 %   generate_autodoc_stubs(in, dest)
+%   generate_autodoc_stubs(in, dest, escape_plus)
 %
 %   % not yet implemented
 %   [success, err_list] = generate_autodoc_stubs(in, dest)
@@ -23,6 +24,8 @@ function [success, err_list] = generate_autodoc_stubs(in, dest)
 %                               set of source files (e.g. 'lib', 'lib/t')
 %                   .names      cell array of function/class names
 %                               (w/o .m extension)
+%           escape_plus         escape '+' in paths in source URLs if true,
+%                               default is false
 %
 %   dest (char array) : path the base directory for symlinks
 %
@@ -57,12 +60,16 @@ function [success, err_list] = generate_autodoc_stubs(in, dest)
 %           '~/matpower/docs/sphinx/source/ref-manual/');
 
 %   MATPOWER
-%   Copyright (c) 2023-2024, Power Systems Engineering Research Center (PSERC)
+%   Copyright (c) 2023-2025, Power Systems Engineering Research Center (PSERC)
 %   by Ray Zimmerman, PSERC Cornell
 %
 %   This file is part of MATPOWER.
 %   Covered by the 3-clause BSD License (see LICENSE file for details).
 %   See https://matpower.org for more info.
+
+if nargin < 3
+    escape_plus = false;
+end
 
 %% check for destination directory
 if ~exist(dest, 'dir')
@@ -135,6 +142,9 @@ for k = 1:length(stub_types)
                 stub_path = relpath;
             else
                 stub_path = sprintf('%s/%s', stub_type.list(m).src_path, relpath);
+            end
+            if escape_plus
+                stub_path = strrep(stub_path, '+', '%2B');
             end
             fprintf(fd, ':raw-html:`<div style="float: right"><a href="%s/%s.m" target=_blank><svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github v-align-middle color-fg-default"><path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path></svg></a></div>`\n', ...
                 stub_type.gh_base_url, stub_path);
